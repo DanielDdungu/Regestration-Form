f49f3d5acf473c2f269874b3e6aec19e0cf8234a
DanielDdungu <daddungu@outlook.com>
2021-09-30 18:54:54 +0200

    if loops

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-alert('Hello World');
+//CONDITIONAL STATEMENTS
+const X = 10;
+if (X == 10) {
+    console.log('X is 10');
+} else {
+    console.log('X is not equal 10');
+}
+
+
+//TO CHECK DATA TYPES
+const Y = 20;
+
+if (Y === 10) {
+    console.log('Y IS 10');
+} else {
+    console.log('Y is not 10');
+}
