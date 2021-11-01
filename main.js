e220481861eb5da3b5b8fae6704687b3a24ad65c
DanielDdungu <daddungu@outlook.com>
2021-11-01 17:36:02 +0100

    Update on Javascript

--- main.js
+++ main.js
@@ -3,3 +3,27 @@ const nameInput = document.querySelector('#name');
 const emailInput = document.querySelector('#email');
 const msg = document.querySelector('.msg');
 const userList = document.querySelector('#users');
+
+
+myForm.addEventListener('submit', onsubmit);
+
+function onsubmit(e) {
+    e.preventDefault();
+
+    if (nameInput.value === '' || emailInput === '') {
+        msg.classList.add('error');
+        msg.innerHTML = 'Please enter all fields';
+
+        setTimeout(() => msg.remove(), 3000);
+    } else {
+        const li = document.createElement('li');
+        li.appendChild(document.createTextNode(`${nameInput.value} : ${emailInput.value}`));
+        userList.appendChild(li);
+
+        //clear fields
+        nameInput.value = '';
+        emailInput.value = '';
+    }
+
+
+}
