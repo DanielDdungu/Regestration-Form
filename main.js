c3d1ee1f2101810d6573475ad35fd4de57223c9a
DanielDdungu <daddungu@outlook.com>
2021-11-01 17:12:38 +0100

    Fuctionality

---
+++ main.js
@@ -0,0 +1,5 @@
+const myForm = document.querySelector('#my-form');
+const nameInput = document.querySelector('#name');
+const emailInput = document.querySelector('#email');
+const msg = document.querySelector('.msg');
+const userList = document.querySelector('#users');
